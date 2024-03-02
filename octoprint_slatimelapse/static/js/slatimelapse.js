3a652c56f9f328c0d7943f3b674ed72be5b2775b
xmodpt <xmodpt@gmail.com>
2024-03-02 21:17:20 +0000

    Update slatimelapse.js

--- octoprint_slatimelapse/static/js/slatimelapse.js
+++ octoprint_slatimelapse/static/js/slatimelapse.js
@@ -15,21 +15,24 @@ $(function() {
         self.goToUrl = function() {
             self.currentUrl(self.newUrl());
         };
-
+		self.onToggleTimelapseEnable = function() {
+            self.settings.settings.plugins.slatimelapse.enabled(!self.settings.settings.plugins.slatimelapse.enabled());
+            self.settings.saveData();
+        };
         // This will get called before the HelloWorldViewModel gets bound to the DOM, but after its depedencies have
         // already been initialized. It is especially guaranteed that this method gets called _after_ the settings
         // have been retrieved from the OctoPrint backend and thus the SettingsViewModel been properly populated.
         self.onBeforeBinding = function() {
             self.newUrl(self.settings.settings.plugins.helloworld.url());
             self.goToUrl();
-        }
+        };
     }
 
     // This is how our plugin registers itself with the application, by adding some configuration information to
     // the global variable ADDITIONAL_VIEWMODELS
     ADDITIONAL_VIEWMODELS.push([
         // This is the constructor to call for instantiating the plugin
-        HelloWorldViewModel,
+        SlaTimelapse,
 
         // This is a list of dependencies to inject into the plugin, the order which you request here is the order
         // in which the dependencies will be injected into your view model upon instantiation via the parameters
@@ -37,6 +40,6 @@ $(function() {
         ["loginStateViewModel", "settingsViewModel"],
 
         // Finally, this is the list of all elements we want this view model to be bound to.
-        [document.getElementById("tab_plugin_helloworld")]
+        ["#navbar_plugin_slatimelapse"]
     ]);
 });
