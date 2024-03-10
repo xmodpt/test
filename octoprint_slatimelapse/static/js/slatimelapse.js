087f034f4f9d171748f04d2d74d448ebe1319355
jneilliii <jneilliii+github@gmail.com>
2024-03-10 14:08:03 -0400

    fix invalid js

--- octoprint_slatimelapse/static/js/slatimelapse.js
+++ octoprint_slatimelapse/static/js/slatimelapse.js
@@ -5,28 +5,10 @@ $(function() {
         self.loginState = parameters[0];
         self.settings = parameters[1];
 
-        // this will hold the URL currently displayed by the iframe
-        self.currentUrl = ko.observable();
-
-        // this will hold the URL entered in the text field
-        self.newUrl = ko.observable();
-
-        // this will be called when the user clicks the "Go" button and set the iframe's URL to the entered URL
-        self.goToUrl = function() {
-            self.currentUrl(self.newUrl());
-        };
-		self.onToggleTimelapseEnable = function() {
-			//console.log("current setting: " + self.settings.settings.plugins.slatimelapse.enabled());
+        self.onToggleTimelapseEnable = function() {
+            // console.log("current setting: " + self.settings.settings.plugins.slatimelapse.enabled());
             self.settings.settings.plugins.slatimelapse.enabled(!self.settings.settings.plugins.slatimelapse.enabled());
             self.settings.saveData();
-        };
-        // This will get called before the HelloWorldViewModel gets bound to the DOM, but after its depedencies have
-        // already been initialized. It is especially guaranteed that this method gets called _after_ the settings
-        // have been retrieved from the OctoPrint backend and thus the SettingsViewModel been properly populated.
-        self.onBeforeBinding = function() {
-            self.newUrl(self.settings.settings.plugins.helloworld.url());
-            self.goToUrl();
-        }
         };
     }
 
@@ -35,7 +17,6 @@ $(function() {
     ADDITIONAL_VIEWMODELS.push([
         // This is the constructor to call for instantiating the plugin
         slatimelapseViewModel,
-	//SlaTimelapse,
 
         // This is a list of dependencies to inject into the plugin, the order which you request here is the order
         // in which the dependencies will be injected into your view model upon instantiation via the parameters
